e328e6ead03305cd03efb46ad61be6e6e6a8215c
Jakub Szwachła <meszkax@gmail.com>
2014-04-26 14:42:01 +0200

    Experimenting with gravity in water

--- game.js
+++ game.js
@@ -1,15 +1,13 @@
+var terrain, water, fish;
 var Game = function () {
-    var terrain, water;
-
     this.setup = function () {
         scaleSetup(2);
         water = jaws.Sprite({
             color: 'blue',
             x:0,
-            y: jaws.height,
-            anchor: 'bottom_left',
+            y: jaws.height - 60,
             width: 320,
-            height: 45
+            height: 60,
         });
         terrain = jaws.PixelMap({ image: 'images/map1.png' })
         fish = jaws.Sprite({
@@ -19,36 +17,64 @@ var Game = function () {
             width: 8,
             height: 8
         });
-        fish.gravity = 1;
-        fish.jumpv = 10;
+        fish.gravity = 0.5;
+        fish.buo = 1;
+        fish.jumpv = 7;
+        fish.swimv = 1;
         fish.vy = 0;
-        fish.vx = 1;
+        fish.max_vy = 8;
+        fish.vx = 0;
+        fish.inWater = function () {
+            return jaws.collide(fish, water);
+        };
+        fish.depth = function () {
+            var d = fish.y - water.y;
+            return d > 0 ? d : 0;
+        };
+        fish.vdamp = function () {
+            // var damp = 0.1 * fish.depth();
+            // return damp <= 0.8 ? 1 - damp : 0.2;
+            if (fish.depth()) {
+                return 0.5;
+            } else {
+                return 1;
+            }
+        };
     };
 
     this.update = function () {
-        if (jaws.pressed("up") && !fish.jumping) {
+        if (jaws.pressed("up") && !fish.jumping && !fish.depth()) {
             fish.vy -= fish.jumpv;
             fish.jumping = true;
         }
+        if (jaws.pressed("up") && fish.depth()) {
+            fish.vy = -1;
+        }
+        // if (fish.inWater()) {
+        //     if (fish.depth() > 0 && fish.vy > 0) {
+        //         fish.vy -= fish.buo * fish.depth();
+        //     }
+        // }
         fish.vy += fish.gravity;
-        fish.y += fish.vy;
-        if (terrain.solidAtRect(fish.rect())) {
-            fish.y -= fish.vy;
+        if (fish.vy > fish.max_vy) fish.vy = fish.max_vy;
+
+        if (jaws.pressed("left") && (fish.jumping || fish.inWater())) {
+            fish.vx -= fish.swimv;
+        }
+        if (jaws.pressed("right") && (fish.jumping || fish.inWater())) {
+            fish.vx += fish.swimv;
+        }
+
+        var collision = fish.stepWhile(fish.vx, fish.vy * fish.vdamp(), function (fish) {
+           return !terrain.solidAtRect(fish.rect())
+        });
+        if (collision.y) {
             fish.vy = 0;
             fish.jumping = false;
         }
-
-        oldx = fish.x;
-        if (jaws.pressed("left") && fish.jumping) {
-            fish.x -= fish.vx;
+        if (collision.x) {
         }
-        if (jaws.pressed("right") && fish.jumping) {
-            fish.x += fish.vx;
-        }
-        if (terrain.solidAtRect(fish.rect())) {
-            fish.x = oldx;
-        }
-
+        fish.vx = 0;
     };
 
     this.draw = function () {
